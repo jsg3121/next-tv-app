4badb120e5199a076c896727167e9fe867adb9ae
jsg3121 <xodm95@gmail.com>
2022-02-03 16:30:29 +0900

    FEAT(init) : first init

---
+++ container/src/test.container.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next';
+
+interface testContainerProps{}
+
+export const testContainer:NextPage = (props)=> {
+  const {} = props
+
+  return (<></>)
+}
---
+++ pages/main/introduce.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+interface introduce{}
+
+const introduce:NextPage = (props)=> {
+  const {} = props
+
+  return (<>자기소개</>)
+}
+
+export default introduce
