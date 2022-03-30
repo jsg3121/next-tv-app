05727f36171e9c03c8c847478c2a7f5c6634c67e
jsg3121 <xodm95@gmail.com>
2022-03-30 13:51:33 +0900

    FEAT(intro) : 인트로 페이지 효과 추가

--- pages/main/intro.tsx
+++ pages/main/intro.tsx
@@ -1,9 +1,58 @@
 import { IntorContainer, NoticeContainer } from 'container'
 import { gsap, Power4 } from 'gsap'
 import type { NextPage } from 'next'
-import { useRouter } from 'next/router'
+import { NextRouter, useRouter } from 'next/router'
 import React from 'react'
 import intro from 'styles/intro.module.scss'
+
+const turnOff = (current: HTMLHeadingElement, router: NextRouter): void => {
+  gsap
+    .timeline()
+    .to(current, {
+      width: '100%',
+      height: '2px',
+      ease: Power4.easeOut,
+      duration: 0.2,
+    })
+    .to(current, {
+      width: '0',
+      height: '0',
+      duration: 0.2,
+    })
+    .then(() => {
+      router.push('/main/introduce')
+    })
+}
+
+const turnOnOff = (current: HTMLHeadingElement) => {
+  const timeLine = gsap.timeline()
+
+  timeLine
+    .to(current, {
+      width: '100%',
+      height: '2px',
+      ease: Power4.easeOut,
+      duration: 0.2,
+    })
+    .to(current, {
+      width: '0',
+      height: '0',
+      duration: 0.2,
+    })
+    .to(current, {
+      width: '100%',
+      height: '2px',
+      ease: Power4.easeOut,
+      duration: 0.2,
+      backgroundImage: 'none',
+    })
+    .to(current, {
+      width: '100%',
+      height: 'calc(100vh - 140px)',
+      duration: 0.2,
+    })
+    .delay(1)
+}
 
 const Intro: NextPage = () => {
   const [isShow, setIsShow] = React.useState<boolean>(false)
@@ -11,55 +60,22 @@ const Intro: NextPage = () => {
   const router = useRouter()
 
   const handleClick = React.useCallback(() => {
-    gsap
-      .timeline()
-      .to(mainRef.current, {
-        width: '100%',
-        height: '2px',
-        ease: Power4.easeOut,
-        duration: 0.2,
-      })
-      .to(mainRef.current, {
-        width: '0',
-        height: '0',
-        duration: 0.2,
-      })
-      .then(() => {
-        router.push('/main/introduce')
-      })
+    if (mainRef.current) {
+      turnOff(mainRef.current, router)
+    }
   }, [router])
 
   React.useEffect(() => {
-    const timeLine = gsap.timeline()
+    if (mainRef.current) {
+      turnOnOff(mainRef.current)
+      const timeSet = setInterval(() => {
+        setIsShow(true)
+      }, 1400)
 
-    timeLine
-      .to(mainRef.current, {
-        width: '100%',
-        height: '2px',
-        ease: Power4.easeOut,
-        duration: 0.2,
-      })
-      .to(mainRef.current, {
-        width: '0',
-        height: '0',
-        duration: 0.2,
-      })
-      .to(mainRef.current, {
-        width: '100%',
-        height: '2px',
-        ease: Power4.easeOut,
-        duration: 0.2,
-      })
-      .to(mainRef.current, {
-        width: '100%',
-        height: '100vh',
-        duration: 0.2,
-      })
-      .delay(1)
-
-    const timeSet = setInterval(() => {
-      setIsShow(true)
-    }, 1400)
+      return () => {
+        return clearInterval(timeSet)
+      }
+    }
   }, [])
 
   return (
