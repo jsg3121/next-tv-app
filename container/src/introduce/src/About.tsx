5708afc532e81c1fb72e13e72955b5fc913ac23e
jsg3121 <xodm95@gmail.com>
2022-03-31 15:44:06 +0900

    FIX(introduce) : 컨셉에 맞게 수정

--- container/src/introduce/src/About.tsx
+++ container/src/introduce/src/About.tsx
@@ -1,16 +1,38 @@
-import React from 'react'
-import isEqual from 'fast-deep-equal'
 import { Text, Title } from 'components'
+import isEqual from 'fast-deep-equal'
+import { gsap } from 'gsap'
 import Image from 'next/image'
-import about from 'styles/about.module.scss'
 import Link from 'next/link'
+import React from 'react'
+import about from 'styles/about.module.scss'
 
 const About: React.FC = () => {
+  const pictureRef = React.useRef<HTMLHeadingElement>(null)
+  const spanRef = React.useRef<HTMLHeadingElement>(null)
+
+  React.useEffect(() => {
+    gsap.timeline().to(pictureRef.current, {
+      y: 0,
+      duration: 2,
+      ease: 'bounce.out',
+    })
+
+    gsap
+      .timeline()
+      .to(spanRef.current, {
+        y: 0,
+        duration: 1,
+        ease: 'bounce.out',
+      })
+      .delay(1)
+  }, [])
+
   return (
     <article className={about.container} id="about">
       <div>
+        <Title depth={1}>About Me</Title>
         <div className={about.profile_info}>
-          <picture className={about.profile}>
+          <picture className={about.profile} ref={pictureRef}>
             <figure>
               <Image
                 src="/profile.webp"
@@ -18,20 +40,45 @@ const About: React.FC = () => {
                 layout="responsive"
                 width="300px"
                 height="300px"
-                loading="lazy"
+                priority
               />
             </figure>
           </picture>
           <ul className={about.info_list}>
             <li>
-              <Title depth={3}>Name&nbsp;:&nbsp;</Title>
-              <Text>장선규 (Jang Sungyu)</Text>
+              <Title depth={3}>NAME</Title>
+              <Text>&nbsp;:&nbsp;장선규 (Jang Sungyu)</Text>
             </li>
             <li>
-              <Title depth={3}>Birth&nbsp;:&nbsp;</Title>
-              <Text>1995. 08. 09</Text>
+              <Title depth={3}>BIRTH</Title>
+              <Text>&nbsp;:&nbsp;1995. 08. 09</Text>
             </li>
             <li>
+              <Title depth={3}>VELOG</Title>
+              <Link href="https://velog.io/@jsg3121">
+                <a target="_blank">&nbsp;:&nbsp;https://velog.io/@jsg3121</a>
+              </Link>
+            </li>
+            <li>
+              <Title depth={3}>WAKATIME</Title>
+              <Link href="https://wakatime.com/@jsg3121">
+                <a target="_blank">
+                  &nbsp;:&nbsp;https://wakatime.com/@jsg3121
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Title depth={3}>CAREER</Title>
+              <Text>&nbsp;:&nbsp;2019. 11 ~ ing - Quber Web Developer</Text>
+            </li>
+            <li>
+              <Title depth={3}> </Title>
+              <Text>
+                &nbsp;&nbsp;&nbsp;2018. 06 ~ 2019. 07 - Cresector Web Developer
+              </Text>
+            </li>
+
+            {/* <li>
               <Title depth={3}>Git&nbsp;:&nbsp;</Title>
               <Link href="https://github.com/jsg3121">
                 <a target="_blank">https://github.com/jsg3121</a>
@@ -43,36 +90,36 @@ const About: React.FC = () => {
                 <a target="_blank">xodm95@gmail.com</a>
               </Link>
             </li>
-            <li>
-              <Title depth={3}>Velog&nbsp;:&nbsp;</Title>
-              <Link href="https://velog.io/@jsg3121">
-                <a target="_blank">https://velog.io/@jsg3121</a>
-              </Link>
-            </li>
-            <li>
-              <Title depth={3}>Wakatime&nbsp;:&nbsp;</Title>
-              <Link href="https://wakatime.com/@jsg3121">
-                <a target="_blank">https://wakatime.com/@jsg3121</a>
-              </Link>
-            </li>
+            */}
           </ul>
         </div>
         <Text>
-          주로 즐겨하던 게임들은 대부분 진행 난이도가 높아 입문하기 어려운 게임
-          종류였습니다. <br />
-          룰을 이해하고 노력한 뒤 발전된 플레이를 하여 한 단계씩 성장하는
-          캐릭터를 보며 성취감과 뿌듯함을 느낄 수 있기 때문입니다.
+          <span ref={spanRef}>
+            주로 즐겨하던 게임들은 대부분 진행 난이도가 높아 입문하기 어려운
+            게임 종류였습니다.
+          </span>
+          <br />
+          <span ref={spanRef}>
+            룰을 이해하고 노력한 뒤 발전된 플레이를 하여 한 단계씩 성장하는
+            캐릭터를 보며 성취감과 뿌듯함을 느낄 수 있기 때문입니다.
+          </span>
           <br />
           <br />
-          개발이란 과정도 목표를 향해 이해하고 시도하며 성장해, 완성된 결과물을
-          보았을 때 다가오는 성취감과 뿌듯함은 게임을 할 때와 같이 매우
-          소중합니다. <br />
-          여러 경험들을 거쳐 이제는 아무것도 없는 공간에, 협업하는 이들의 모든
-          노력들을 표현할 수 있는 기술을 가졌습니다.
+          <span ref={spanRef}>
+            개발이란 과정도 목표를 향해 이해하고 시도하며 성장해, 완성된
+            결과물을 보았을 때 다가오는 성취감과 뿌듯함은 게임을 할 때와 같이
+            매우 소중합니다.
+          </span>
           <br />
+          <span ref={spanRef}>
+            여러 경험들을 거쳐 이제는 아무것도 없는 공간에, 협업하는 이들의 모든
+            노력들을 표현할 수 있는 기술을 가졌습니다.
+          </span>
           <br />
-          스스로 더욱 발전하며 즐거운 성취감을 느낄 수 있도록 더욱 높은 곳을
-          위해 끈임없이 도전하고 노력해 나아가겠습니다.
+          <span ref={spanRef}>
+            스스로 더욱 발전하며 즐거운 성취감을 느낄 수 있도록 더욱 높은 곳을
+            위해 끈임없이 도전하고 노력해 나아가겠습니다.
+          </span>
         </Text>
       </div>
     </article>
--- pages/main/introduce.tsx
+++ pages/main/introduce.tsx
@@ -1,10 +1,15 @@
+import http from 'axios'
 import { About, Contact, Project } from 'container'
+import { gsap, Power4 } from 'gsap'
 import type { NextPage } from 'next'
+import React from 'react'
 import layout from 'styles/introduce.module.scss'
 import useSWR from 'swr'
-import http from 'axios'
 
 const Introduce: NextPage = () => {
+  const introduceRef = React.useRef<HTMLHeadingElement>(null)
+  const openningRef = React.useRef<HTMLHeadingElement>(null)
+
   const { data: projectData } = useSWR('/intorduce/project', async () => {
     return await http
       .request({
@@ -14,8 +19,35 @@ const Introduce: NextPage = () => {
       .then((res) => res.data)
   })
 
+  React.useEffect(() => {
+    // gsap
+    //   .timeline()
+    //   .to(introduceRef.current, {
+    //     width: '0',
+    //     height: '0',
+    //     duration: 0,
+    //     overflow: 'hidden',
+    //   })
+    //   .to(introduceRef.current, {
+    //     width: '100%',
+    //     height: '2px',
+    //     ease: Power4.easeOut,
+    //     duration: 0.1,
+    //     overflow: 'hidden',
+    //   })
+    //   .to(introduceRef.current, {
+    //     width: '100%',
+    //     height: '100vh',
+    //     duration: 0.1,
+    //     overflowY: 'auto',
+    //   })
+  }, [])
+
   return (
-    <section className={layout.section}>
+    <section className={layout.section} ref={introduceRef}>
+      <div ref={openningRef}>
+        <h1>ABOUT ME</h1>
+      </div>
       <section>
         <About />
       </section>
