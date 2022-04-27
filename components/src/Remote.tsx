1f40acebc9168f5d3a67caab7472fb34d7fdcea3
jsg3121 <xodm95@gmail.com>
2022-04-27 10:37:54 +0900

    FEAT(remote) : 리모컨 컴포넌트 추

--- components/src/Remote.tsx
+++ components/src/Remote.tsx
@@ -1,11 +1,63 @@
 import React from 'react'
 import isEqual from 'fast-deep-equal'
 import remote from 'styles/remote.module.scss'
+import Image from 'next/image'
+import styled from 'styled-components'
 
 interface RemoteProps {}
 
+const Lights = styled.div`
+  width: 0.8rem;
+  height: 0.3rem;
+  border-radius: 1.5rem;
+  position: absolute;
+  background-color: #afafaf;
+  right: 0;
+  box-shadow: 0.4px 0.8px 1px -0.2px #888888, inset 0.5px 0.3px 1px #ffffff;
+
+  &.active {
+    background-color: #a5e741 !important;
+  }
+`
+
 const Remote: React.FC<RemoteProps> = (props) => {
   const {} = props
+  const activeRef = React.useRef<HTMLDivElement>(null)
+
+  const handleSignal = React.useCallback(() => {
+    if (activeRef.current) {
+      activeRef.current.classList.add('active')
+    }
+  }, [])
+
+  const handleOff = React.useCallback(() => {
+    if (activeRef.current) {
+      activeRef.current.classList.remove('active')
+    }
+  }, [])
+
+  const themeImg = React.useMemo(() => {
+    return (
+      <>
+        <i>
+          <Image
+            src="/theme_light.svg"
+            layout="fill"
+            alt="icon_theme"
+            priority
+          />
+        </i>
+        <i>
+          <Image
+            src="/theme_dark.svg"
+            layout="fill"
+            alt="icon_theme"
+            priority
+          />
+        </i>
+      </>
+    )
+  }, [])
 
   return (
     <>
@@ -16,18 +68,47 @@ const Remote: React.FC<RemoteProps> = (props) => {
               <i></i>
             </button>
           </div>
-          <div className="battery">
+          <Lights ref={activeRef}>
             <i></i>
-          </div>
+          </Lights>
         </div>
         <div className={remote.channel_btn}>
           <ul className={remote.btn_container}>
-            <li className="ch_up"></li>
-            <li className="ch_right"></li>
-            <li className="ch_down"></li>
-            <li className="ch_left"></li>
-            <li className="ch_ok">OK</li>
+            <li
+              onMouseDown={handleSignal}
+              onMouseUp={handleOff}
+              className="ch_up"
+            ></li>
+            <li
+              onMouseDown={handleSignal}
+              onMouseUp={handleOff}
+              className="ch_right"
+            ></li>
+            <li
+              onMouseDown={handleSignal}
+              onMouseUp={handleOff}
+              className="ch_down"
+            ></li>
+            <li
+              onMouseDown={handleSignal}
+              onMouseUp={handleOff}
+              className="ch_left"
+            ></li>
+            <li
+              onMouseDown={handleSignal}
+              onMouseUp={handleOff}
+              className="ch_ok"
+            >
+              OK
+            </li>
           </ul>
+        </div>
+        <div className={remote.switch_theme}>
+          <input type="checkbox" name="theme" id="theme" />
+          <label htmlFor="theme">
+            <i></i>
+          </label>
+          <div className={remote.theme_icon}>{themeImg}</div>
         </div>
         {/* <div className="keypad">
           <ul>
