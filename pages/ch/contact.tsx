634198119a535f1464c3eb0b2329e0991a123869
jsg3121 <xodm95@gmail.com>
2022-05-10 17:58:07 +0900

    #14 Contact ui 추가

---
+++ pages/ch/contact.tsx
@@ -0,0 +1,63 @@
+import type { NextPage } from 'next'
+import { gsap } from 'gsap'
+import page from 'styles/page.module.scss'
+import { Title } from 'components'
+
+const Contact: NextPage = (props) => {
+  return (
+    <div className={page.section_contact}>
+      <ul>
+        <li>
+          <Title depth={1}>Cast</Title>
+        </li>
+        <li>
+          <p>asd.flahwelvkjasbcvlkjh</p>
+        </li>
+        <li>
+          <p>asd.flahwelvkjasbcvlkjh</p>
+        </li>
+        <li>
+          <p>asd.flahwelvkjasbcvlkjh</p>
+        </li>
+        <li>
+          <p>asd.flahwelvkjasbcvlkjh</p>
+        </li>
+        <li>
+          <p>asd.flahwelvkjasbcvlkjh</p>
+        </li>
+        <li>
+          <p>asd.flahwelvkjasbcvlkjh</p>
+        </li>
+        <li>
+          <p>asd.flahwelvkjasbcvlkjh</p>
+        </li>
+        <li>
+          <p>asd.flahwelvkjasbcvlkjh</p>
+        </li>
+        <li>
+          <p>asd.flahwelvkjasbcvlkjh</p>
+        </li>
+        <li>
+          <p>asd.flahwelvkjasbcvlkjh</p>
+        </li>
+        <li>
+          <p>asd.flahwelvkjasbcvlkjh</p>
+        </li>
+        <li>
+          <p>asd.flahwelvkjasbcvlkjh</p>
+        </li>
+        <li>
+          <p>asd.flahwelvkjasbcvlkjh</p>
+        </li>
+        <li>
+          <p>asd.flahwelvkjasbcvlkjh</p>
+        </li>
+        <li>
+          <p>asd.flahwelvkjasbcvlkjh</p>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Contact
